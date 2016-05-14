6776084bcb63b4d71d57df78ac65dc3b0bacf77c
Angel Trinh <trinh.angel@gmail.com>
2016-05-14 15:37:26 -0400

    updated heroku hopefully this works

--- server.js
+++ server.js
@@ -42,5 +42,6 @@ app.set('view engine', 'handlebars');
 var routes = require('./controllers/burgers_controller.js');
 app.use('/', routes);
 
-var port = 3000;
+//look to see if the server has a port for us, if not go to 3000
+var port = process.env.PORT || 3000;
 app.listen(port);
