7ec105dda11b1207173b93a4a46b4726f42b4a3c
Angel Trinh <trinh.angel@gmail.com>
2016-05-16 15:18:09 -0400

    updated css

--- config/connection.js
+++ config/connection.js
@@ -1,29 +1,29 @@
 var mysql = require('mysql');
 
-var connection;
-if (process.env.JAWSDB_URL){
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
+// var connection;
+// if (process.env.JAWSDB_URL){
+// 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-} else{
-	connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'burgers_db'
-
-
-	});
-
-
-};
-
-// locahost
-// var connection = mysql.createConnection({
+// } else{
+// 	connection = mysql.createConnection({
 //     host: 'localhost',
 //     user: 'root',
 //     password: '',
 //     database: 'burgers_db'
-// });
+
+
+// 	});
+
+
+// };
+
+// locahost
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'burgers_db'
+});
 
 
 // connection.connect(function(err) {
